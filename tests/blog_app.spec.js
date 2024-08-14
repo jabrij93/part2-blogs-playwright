067cdd21f5fffa4a1816cdcc6eef10884ae35e63
jabss <jabrijuhinin93@gmail.com>
2024-08-15 02:13:33 +0800

    login form test is displayed(test configured correctly)

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,11 +1,25 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
     await page.goto('http://localhost:5173')
   })
 
   test('Login form is shown', async ({ page }) => {
-    // ...
+    await page.goto('http://localhost:5173')
+    
+    const locator = await page.getByText('Blogs')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('Blog app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      // ...
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      // ...
+    })
   })
 })

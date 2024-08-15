09529be6fbee47228f1baa150431ad968abb62ad
jabss <jabrijuhinin93@gmail.com>
2024-08-16 00:45:51 +0800

    add db reset request and create 1 user(fixed when initializing db in testing environment)

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -2,6 +2,15 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
